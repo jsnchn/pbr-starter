20c9fc33b66364d766dbf7558b23e2efe78038ba
Jason Chen <jason@theweb.team>
2025-06-19 22:42:42 -0700

    chore: update dependencies and refactor PocketBase configuration

--- main.go
+++ main.go
@@ -2,20 +2,26 @@ package main
 
 import (
     "log"
+    "net/http"
+    "os"
 
     "github.com/pocketbase/pocketbase"
+    "github.com/pocketbase/pocketbase/apis"
     "github.com/pocketbase/pocketbase/core"
-    "github.com/labstack/echo/v5"
 )
 
 func main() {
     app := pocketbase.New()
 
-    app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
-        e.Router.GET("/api/hello", func(c echo.Context) error {
-            return c.JSON(200, map[string]string{"message": "Hello from PocketBase!"})
-        })
-        return nil
+    app.OnServe().BindFunc(func(se *core.ServeEvent) error {
+        // serves static files from the provided public dir (if exists)
+        se.Router.GET("/{path...}", apis.Static(os.DirFS("./pb_public"), false))
+
+        se.Router.GET("/api/hello", func(e *core.RequestEvent) error {
+            return e.JSON(http.StatusOK, map[string]string{"message": "Hello from PocketBase!"})
+        }).Bind(apis.RequireAuth())
+
+        return se.Next()
     })
 
     if err := app.Start(); err != nil {
