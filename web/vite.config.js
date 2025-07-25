daed3be752c228d478319b168c45ab9cee13f183
Jason Chen <jchen.json@gmail.com>
2025-07-25 00:01:18 -0700

    refactor scaffold

---
+++ web/vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8090',
+        changeOrigin: true,
+      },
+      '/_': {
+        target: 'http://127.0.0.1:8090',
+        changeOrigin: true,
+      }
+    }
+  },
+  build: {
+    outDir: '../server/pb_public',
+    emptyOutDir: true,
+  }
+})
